b328fcc7c895888e118e712572d892768330996a
Hosam <108076964+Hosam89@users.noreply.github.com>
2023-01-26 15:30:11 +0100

    123

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { Swiper, SwiperSlide } from "swiper/react/swiper-react";
+import { Swiper, SwiperSlide } from "swiper/react/swiper-react.d.ts";
 import { FreeMode } from "swiper";
 import PlayPause from "./PlayPause";
 import { playPause, setActiveSong } from "../redux/features/playerSlice";
