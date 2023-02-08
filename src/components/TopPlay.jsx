2f845bdde8d5c1795375496f0c4d2f8ec659441b
Hosam <108076964+Hosam89@users.noreply.github.com>
2023-02-08 14:03:23 +0100

    1233

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -1,17 +1,3 @@
-import { useEffect, useRef } from "react";
-import { Link } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { SwiperSlide } from "Swiper/react";
-import { Swiper } from "Swiper/react";
-import { FreeMode } from "swiper";
-import PlayPause from "./PlayPause";
-import { playPause, setActiveSong } from "../redux/features/playerSlice";
-
-import "swiper/css";
-import "swiper/css/free-mode";
-
-import { useGetTopChartsQuery } from "../redux/services/shazamCore";
-
 const TopPlay = () => {
   return <div></div>;
 };
